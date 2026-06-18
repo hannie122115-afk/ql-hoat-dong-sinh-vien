bedee0bcf3770c5d2b5017ed8e7e54cc6405e56e
pphannie <nguyenhanngoc8@gmail.com>
2026-06-18 15:04:14 +0700

    API cho phần tìm kiếm

--- includes/search.php
+++ includes/search.php
@@ -1,39 +1,40 @@
-<?php 
+<?php
 
-require_once "config/db.php";
+require_once "../config/db.php";
+header('Content-Type: application/json');
 
-$q = $_GET['q'] ?? ''; //lấy ký tự gõ
-$type = $_GET['type'] ?? '';
+if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['keyword']) && isset($_POST['type'])){
+    $keyword = trim($_POST['keyword']);
+    $type = $_POST['type'];
 
-$search = "%$q%";
+    if(!empty($keyword)){
+        $search = "%$keyword%";
+    } else{
+        echo json_encode([]); //tra ve mang json rong
+        exit;
+    }
+    
 
-switch($type){
-    case "unit":
-        $sql = "SELECT * FROM DonVi WHERE TenDonVI LIKE ?";
-        break;
+    switch($type){
+        case "unit":
+            $sql = "SELECT 
+                        MaDonVi as id
+                        , TenDonVi as name
+                        FROM DonVi
+                        WHERE TenDonVi LIKE ?
+                        LIMIT 8";
+            break;
+            // demo trước 1 bảng , còn lại sẽ thêm vào sau nếu luống đi đúng
+        default:
+            echo json_encode([]);
+            exit;
+    }
 
-    default:
-    die("Invalid type");
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$search]); //execute chỉ nhận dạng mảng
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($data);
+
 }
 
-$stmt = $conn->prepare($sql); // chuẩn bị dữ liệu
-$stmt->bind_param("s", $search); // gắn dữ liệu vào phần ? trong sql
-$stmt->execute();
-
-$result = $stmt->get_result();
-
 ?>
-
-<!-- Hiện kết quả  -->
-
-<?php while ($row = $result->fetch_assoc()): ?>
-    <div>
-        <?php 
-        
-        switch($type){
-            case "unit":
-                $row['TenDonVi'];
-        }
-        ?>
-    </div>
-<?php endwhile; ?>
