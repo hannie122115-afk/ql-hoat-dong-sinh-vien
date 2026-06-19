0e619f1c8e03e1568d5630bec7c4b8bbf77f86b2
pphannie <nguyenhanngoc8@gmail.com>
2026-06-19 23:08:59 +0700

    xong rigister, login, logout

--- login.php
+++ login.php
@@ -5,6 +5,48 @@ if(session_status() === PHP_SESSION_NONE){
 
 require_once "config/db.php";
 
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+  $email = trim($_POST['email'] ?? '');
+  $password = $_POST['password'] ?? '';
+  $error = [];
+
+  if(empty($email)){
+    $error['email'] = 'Vui lòng nhập email đăng nhập.';
+  }
+
+  if(empty($password)){
+    $error['password'] = 'Vui lòng nhập mật khẩu đăng nhập.';
+  }
+
+  if(empty($error['email'])){
+    $sql = "SELECT * FROM taikhoandangnhap WHERE email = ? LIMIT 1";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$email]);
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC); // biến này nếu không có sẽ cho ra false
+
+    if($user === false){
+      $error['email'] = "Email không tồn tại trên hệ thống!";
+    }else{
+      if(password_verify($password, $user['MatKhau'])){
+        $_SESSION['user_id'] = $user['MaTaiKhoan'];
+        $_SESSION['email'] = $user['Email'];
+        if((int)$user['Role'] == 0){
+          header("Location: user/homepage.php");
+          exit;
+        } else{
+          header("Location: admin/homepage.php");
+          exit;
+        }
+      } else{
+        $error['password'] = "Mật khẩu không chính xác. Vui lòng thử lại.";
+      }
+    }
+  }
+  
+}
+
 
 
 ?>
@@ -25,6 +67,12 @@ require_once "config/db.php";
           </div>
           <h1>Đăng nhập</h1>
           <small>Đăng nhập để bắt đầu hành trình của bạn</small>
+
+          <?php if(isset($_SESSION['success_message'])){ ?>
+            <div class="success_login_message">
+              <?= $_SESSION['success_message']; ?>
+            </div>
+          <?php unset($_SESSION['success_message']); } ?>
 
           <div class="login-block">
             <div class="login-title-block">
